3fca46a251152509215f90137de3db6951249f54
Seunghyun Yang <ggyool@naver.com>
2019-12-11 03:39:49 +0900

    finished chapter8

---
+++ Part2/seunghyun/ex01/src/main/java/org/zerock/domain/EXAMPLE.java
@@ -0,0 +1,8 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class EXAMPLE {
+	int exn;
+}
---
+++ Part3/Seunghyun/ex02/src/test/java/org/ggyool/mapper/BoardMapperTests.java
@@ -0,0 +1,21 @@
+package org.ggyool.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {org.ggyool.config.RootConfig.class} )
+@Log4j
+public class BoardMapperTests {
+	@Autowired
+	private BoardMapper mapper;
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+}
